82ced2d17c427235af8b3cbfa628f20019dc4530
Luiz <luiz.carlos.limajf@gmail.com>
2019-11-26 15:18:33 -0300

    Add Step1 screen

---
+++ frontend/src/screens/Step1.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+
+export default function Step1() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}></View>
+      <Image
+        style={styles.avatar}
+        source={{ uri: "https://bootdey.com/img/Content/avatar/avatar6.png" }}
+      />
+      <View style={styles.body}>
+        <View style={styles.bodyContent}>
+          <Text style={styles.txtTitle}>Hmmmmm</Text>
+          <Text style={styles.txt}>Você parece que vai ser XXXX.</Text>
+          <Text style={styles.txt}>Vamos conferir?</Text>
+          <TouchableOpacity style={styles.nextIconContainer} onPress={() => {}}>
+            <FontAwesome5 name="chevron-right" size={30} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#00BFFF",
+    height: 200
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 130
+  },
+  body: {
+    marginTop: 40
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: "center",
+    padding: 30
+  },
+  txt: {
+    fontSize: 16,
+    lineHeight: 20,
+    marginVertical: 20
+  },
+  nextIconContainer: {
+    marginTop: 30
+  },
+  txtTitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    lineHeight: 20,
+    marginVertical: 20
+  },
+  navigation: {
+    display: "flex",
+    alignItems: "center",
+    alignContent: "space-between"
+  }
+});
